cfc905b4ba64d9ebcaf29c6fc3155654a8238f80
julianabjort <julianabjortiv@gmail.com>
2022-11-14 18:12:14 +0100

    testing

--- src/pages/stats.tsx
+++ src/pages/stats.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const stats = () => {
-  return <div>stats</div>;
+  return (
+    <div>
+      stats
+      <h2>Helloooooooooooooooo</h2>
+    </div>
+  );
 };
 
 export default stats;
