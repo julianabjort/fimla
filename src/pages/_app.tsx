ae65b349ad87fef51521720fcf64f20db7ddfca6
julianabjort <julianabjortiv@gmail.com>
2022-12-15 13:16:07 +0100

    nextfonts package & google font added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,19 @@ import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import Layout from "../components/Layout";
+import { Manrope } from "@next/font/google";
+
+const manrope = Manrope({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <ThemeProvider attribute="class">
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <main className={manrope.className}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </main>
       </ThemeProvider>
     </SessionProvider>
   );
