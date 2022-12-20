d38b46e77cfebfa1872841e08091e68c8f7f86d2
Laufey Hjaltested <laufeyhjaltested@Laufeys-MacBook-Pro.local>
2022-12-20 01:51:48 +0100

    Stats are working seperately for wordle and quordle and connected to db

--- src/pages/games/quordle.tsx
+++ src/pages/games/quordle.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { observer, useLocalObservable } from "mobx-react-lite";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Keyboard2 from "../../components/Keyboard2";
 import QuordleGrid from "../../components/QuordleGrid";
 import QuordleStore from "../../stores/QuordleStore";
-import play from "../play";
-import { HiMenuAlt1 } from "react-icons/hi";
+import { useSession } from "next-auth/react";
 
 const quordle = () => {
+  const { data: session } = useSession();
+  const [qStats, setQstats] = useState({
+    wins: 0,
+    losses: 0,
+    totalScore: 0,
+    gamesPlayed: 0,
+    avgScore: 0
+  });
   const store = useLocalObservable(() => QuordleStore);
+
   useEffect(() => {
     store.init();
     window.addEventListener("keyup", store.handleKeyup);
@@ -16,7 +24,83 @@ const quordle = () => {
       window.removeEventListener("keyup", store.handleKeyup);
     };
   }, []);
-  // console.log("won ? ", store.win1, store.win2,store.win3,store.win4, "WIN ? ", store.win, store.won)
+
+  const readQuordleStats = async() => {
+    if(session){
+      const userSession = session?.user
+      try{
+        const response = await fetch(`/api/quordle-stats`, {
+          method: "GET",
+          headers: {"Content-Type": "application/json"},
+        });
+        const allStats = await response.json()
+        const qStats = allStats.filter(i => i.userEmail === userSession.email)
+        setQstats(qStats)
+        console.log("my stats: ", qStats)
+        console.log("stats: ", qStats)
+        return qStats
+      } catch (error) {
+        console.log("error: ", error)
+      }
+    } else {
+      console.log("no session")
+    }
+  }
+  
+  useEffect(() => {
+    readQuordleStats();
+  }, [session])
+  
+  const addQuordleStats = async() => {
+    console.log(qStats)
+    readQuordleStats();
+    if(session){
+      const user = session.user.email
+      let wins = 0
+      let losses = 0
+      let totalScore = 0
+      if(!qStats[0]){
+        totalScore = store.totalScore
+        if(store.wonAll){
+          wins =  1 
+        }
+        if(store.lost){
+          losses =  1
+        }
+      } else {
+        const userStats = qStats[0]
+        totalScore = userStats.totalScore + store.totalScore
+        console.log("store: ", store.totalScore)
+        if(store.wonAll){
+          wins = userStats.wins + 1
+        }
+        if(store.lost){
+          losses = userStats.losses + 1
+        }
+      }
+      const body = {user, totalScore, wins, losses}
+      console.log("TOTAL: ", totalScore)
+    try{
+      const response = await fetch(`/api/quordle-stats`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(body)
+      });
+    }catch(error){
+      console.log("error: ", error)
+    }
+    try{
+      const response = await fetch(`/api/quordle-stats`, {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(body)
+      });
+    }catch(error){
+      console.log("error: ", error)
+    }
+  }
+  }
+
   return (
     <div className="flex flex-col items-center my-10 justify-evenly">
       <h1 className="heading-1">Quordle</h1>
@@ -92,6 +176,7 @@ const quordle = () => {
       <h1>word2: {store.word2}</h1>
       <h1>word3: {store.word3}</h1>
       <h1>word4: {store.word4}</h1> */}
+      <button onClick={addQuordleStats}>Save your score!</button>
 
       {/* <button onClick={store.calculateScore}>Save</button>
       <h1>guesses: {JSON.stringify(store.guesses)}</h1>
--- src/pages/stats.tsx
+++ src/pages/stats.tsx
@@ -1,52 +1,120 @@
+import { useSession } from "next-auth/react";
 import React from "react";
 import { useEffect, useState } from "react";
 
+
 const stats = () => {
-  const [stats, setStats] = useState({
-    wins: 0,
-    losses: 0,
-    gamesPlayed: 0,
-    avgScore: 0,
-    avgTurns: 0,
-  });
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      let stats = JSON.parse(localStorage.getItem("stats"));
-      if (stats !== null) setStats(stats);
-      console.log(stats);
+  const { data: session } = useSession();
+  const [stats, setStats] = useState({});
+  const [qStats, setQstats] = useState({});
+  const [wordle, setWordle] = useState(true);
+  const [quordle, setQuordle] = useState(false)
+
+  let wGamesPlayed = stats[0]?.wins + stats[0]?.losses || 0
+  let qGamesPlayed = qStats[0]?.wins + qStats[0]?.losses || 0
+  const wordleClick = () => {
+      setWordle(true)
+      setQuordle(false)
+  }
+  const quordleClick = () => {
+    setQuordle(true)
+    setWordle(false)
+  }
+  const WordleStats = async() => {
+    if(session) {
+      console.log("SESSION")
+      const userSession = session?.user
+      try{
+        const response = await fetch(`/api/wordle-stats`, {
+          method: "GET",
+          headers: {"Content-Type": "application/json"},
+        });
+        const allStats = await response.json()
+        const myStats = allStats.filter(i => i.userEmail === userSession.email)
+          setStats(myStats);
+      } catch (error) {
+          console.log("error: ", error)
+      }
+      try{
+        const response = await fetch(`/api/quordle-stats`, {
+          method: "GET",
+          headers: {"Content-Type": "application/json"},
+        });
+        const allStats = await response.json()
+        const qStats = allStats.filter(i => i.userEmail === userSession.email)
+          setQstats(qStats);
+      } catch (error) {
+          console.log("error: ", error)
+      }
+    } else {
+        console.log("no session")
+        let stats = [JSON.parse(localStorage.getItem("stats"))];
+        if (stats !== null) setStats(stats);
+        console.log(stats);
+      }
     }
-  }, []);
+    
+    useEffect(() => {
+      WordleStats();
+      // console.log(gamesPlayed)
+    }, [session]);
 
   return (
     <div className="flex flex-col gap-y-4">
       <h1 className="my-10 heading-1">Game Stats</h1>
+      <div className="flex flex-row">
+      <button onClick={wordleClick} className={`p-4 pl-0 mr-2 rounded-md `}><h4 className={`border-b-2 ${wordle?"border-black dark:border-white":"border-transparent"}`}>Wordle</h4></button>
+      <button onClick={quordleClick} className={`p-4  mr-2 rounded-md `}><h4 className={`border-b-2 ${quordle?"border-black dark:border-white":"border-transparent"}`}>Quordle</h4></button>
+      </div>
       <section className="flex w-full gap-x-4">
         <div className="flex flex-col justify-between w-full h-56 p-6 rounded-md bg-lighter dark:bg-darker">
           <h2 className="border-b-[0.5px] pb-1 heading-2">Wins</h2>
-          <h3 className="text-7xl">{stats.wins}</h3>
+          {wordle ? (
+            <h3 className="text-7xl">{stats[0]?.wins || 0}</h3>) 
+          : quordle ? (
+            <h3 className="text-7xl">{qStats[0]?.wins || 0}</h3>):(<></>)}
+          {/* <h3 className="text-7xl">{stats[0]?.wins}</h3> */}
         </div>
         <div className="flex flex-col justify-between w-full h-56 p-6 rounded-md bg-lighter dark:bg-darker">
           <h2 className="border-b-[0.5px] pb-1 heading-2">Losses</h2>
-
-          <h3 className="text-7xl">{stats.losses}</h3>
+          {wordle ? (
+            <h3 className="text-7xl">{stats[0]?.losses || "0"}</h3>) 
+          : quordle ? (
+            <h3 className="text-7xl">{qStats[0]?.losses || "0"}</h3>):(<></>)}
+          {/* <h3 className="text-7xl">{stats[0]?.losses}</h3> */}
         </div>
         <div className="flex flex-col justify-between w-full h-56 p-6 rounded-md bg-lighter dark:bg-darker">
           <h2 className="border-b-[0.5px] pb-1  heading-2">Games Played</h2>
-          <h3 className="text-7xl">{stats.gamesPlayed}</h3>
+          {wordle ? (
+            <h3 className="text-7xl">{wGamesPlayed || "0"}</h3>) 
+          : quordle ? (
+            <h3 className="text-7xl">{qGamesPlayed || "0"}</h3>):(<></>)}
+          {/* <h3 className="text-7xl">{gamesPlayed || ""}</h3> */}
         </div>
         <div className="flex flex-col justify-between w-full h-56 p-6 rounded-md bg-lighter dark:bg-darker">
           <h2 className="border-b-[0.5px] pb-1  heading-2">Average Score</h2>
-          <h3 className="text-7xl">{stats.avgScore}</h3>
+          {wordle ? (
+            <h3 className="text-7xl">{stats[0]?.totalScore/wGamesPlayed || "0"}</h3>) 
+          : quordle ? (
+            <h3 className="text-7xl">{qStats[0]?.totalScore/qGamesPlayed || "0"}</h3>):(<></>)}
+          {/* <h3 className="text-7xl">{stats[0]?.totalScore/gamesPlayed || ""}</h3> */}
         </div>
       </section>
       <section className="flex w-full gap-x-4">
         <div className="flex flex-col justify-between w-full h-56 p-6 rounded-md bg-lighter dark:bg-darker">
           <h2 className="border-b-[0.5px] pb-1  heading-2">Win Ratio</h2>
-          <h3 className="text-7xl">{stats.gamesPlayed}</h3>
+          {/* <h3 className="text-7xl">{stats.gamesPlayed}</h3> */}
         </div>
         <div className="flex flex-col justify-between w-full h-56 p-6 rounded-md bg-lighter dark:bg-darker">
           <h2 className="border-b-[0.5px] pb-1  heading-2">Average Turns</h2>
-          <h3 className="text-7xl">{stats.avgTurns}</h3>
+          {/* <h3 className="text-7xl">{stats.avgTurns}</h3> */}
+          {/* showing total score now */}
+          {wordle ? (
+            <h3 className="text-7xl">{stats[0]?.totalScore || "0"}</h3>) 
+          : quordle ? (
+            <h3 className="text-7xl">{qStats[0]?.totalScore || "0"}</h3>):(<></>)}
+          {/* <h3 className="text-7xl">{stats[0]?.totalScore || ""}</h3> */}
+
         </div>
       </section>
     </div>
