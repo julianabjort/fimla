9d2195536d5abf3db9bffa1b319665042b8a94d9
Laufey Hjaltested <laufeyhjaltested@Laufeys-MacBook-Pro.local>
2022-12-29 15:43:17 +0000

    fixing wordle points

--- src/pages/games/wordle.tsx
+++ src/pages/games/wordle.tsx
@@ -13,7 +13,7 @@ const wordle = () => {
     losses: 0,
     totalScore: 0,
     gamesPlayed: 0,
-    avgScore: 0
+    avgScore: 0,
   });
   const store = useLocalObservable(() => WordleStore);
 
@@ -25,82 +25,84 @@ const wordle = () => {
     };
   }, []);
 
-  const readWordleStats = async() => {
-    if(session){
-      const userSession = session?.user
-      try{
+  const readWordleStats = async () => {
+    if (session) {
+      const userSession = session?.user;
+      try {
         const response = await fetch(`/api/wordle-stats`, {
           method: "GET",
-          headers: {"Content-Type": "application/json"},
+          headers: { "Content-Type": "application/json" },
         });
-        const allStats = await response.json()
-        const myStats = allStats.filter(i => i.userEmail === userSession.email)
-        setStats(myStats)
-        console.log("my stats: ", myStats)
-        console.log("stats: ", stats)
-        return myStats
+        const allStats = await response.json();
+        const myStats = allStats.filter(
+          (i) => i.userEmail === userSession.email
+        );
+        setStats(myStats);
+        console.log("my stats: ", myStats);
+        console.log("stats: ", stats);
+        return myStats;
       } catch (error) {
-        console.log("error: ", error)
+        console.log("error: ", error);
       }
     } else {
-      console.log("no session")
+      console.log("no session");
     }
-  }
-  
+  };
+
   useEffect(() => {
     readWordleStats();
-  }, [session])
+  }, [session]);
 
-  const addWordleStats = async() => {
-    console.log(stats)
+  const addWordleStats = async () => {
+    console.log(stats);
     readWordleStats();
-    if(session){
-      const user = session.user.email
-      let wins = 0
-      let losses = 0
-      let totalScore = 0
-      if(!stats[0]){
-        totalScore = store.totalScore
-        if(store.won){
-          wins =  1 
+    if (session) {
+      const user = session.user.email;
+      let wins = stats.wins;
+      let losses = stats.losses;
+      let totalScore = stats.totalScore;
+      if (!stats[0]) {
+        totalScore = store.totalScore;
+        if (store.won) {
+          wins = 1;
         }
-        if(store.lost){
-          losses =  1
+        if (store.lost) {
+          losses = 1;
         }
       } else {
-        const userStats = stats[0]
-        totalScore = userStats.totalScore + store.totalScore
-        console.log("store: ", store.totalScore)
-        if(store.won){
-          wins = userStats.wins + 1
+        const userStats = stats[0];
+        totalScore = userStats.totalScore + store.totalScore;
+        console.log("store: ", store.totalScore);
+        if (store.won) {
+          wins = userStats.wins + 1;
         }
-        if(store.lost){
-          losses = userStats.losses + 1
+        if (store.lost) {
+          losses = userStats.losses + 1;
         }
       }
-      const body = {user, totalScore, wins, losses}
-      console.log("TOTAL: ", totalScore)
-    try{
-      const response = await fetch(`/api/wordle-stats`, {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(body)
-      });
-    }catch(error){
-      console.log("error: ", error)
+      const body = { user, totalScore, wins, losses };
+      console.log("TOTAL: ", totalScore);
+      try {
+        const response = await fetch(`/api/wordle-stats`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body),
+        });
+      } catch (error) {
+        console.log("error: ", error);
+      }
+      try {
+        const response = await fetch(`/api/wordle-stats`, {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body),
+        });
+      } catch (error) {
+        console.log("error: ", error);
+      }
     }
-    try{
-      const response = await fetch(`/api/wordle-stats`, {
-        method: "PUT",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(body)
-      });
-    }catch(error){
-      console.log("error: ", error)
-    }
-  }
-  }
-  console.log("the word: ", store.word)
+  };
+  console.log("the word: ", store.word);
   return (
     <div className="flex flex-col items-center my-10 justify-evenly">
       <h1 className="heading-1">Wordle</h1>
@@ -113,19 +115,21 @@ const wordle = () => {
           key={i}
         />
       ))}
-      {store.won && <h1 className="text-lg font-bold">You won! You are good!</h1>}
-      {store.lost && <div className="flex items-center my-2 gap-x-8">
-            <p className="text-lg font-bold">Almost!  The correct word was:</p>
-            <div>
-              <p className="text-lg font-bold text-green">
-                {store.word} 
-              </p>
-              
-            </div>
-          </div>}
-      {(store.lost || store.won) && (<>
-        <button onClick={store.startGame}>Play again</button>
-        <button onClick={addWordleStats}>Save your score!</button>
+      {store.won && (
+        <h1 className="text-lg font-bold">You won! You are good!</h1>
+      )}
+      {store.lost && (
+        <div className="flex items-center my-2 gap-x-8">
+          <p className="text-lg font-bold">Almost! The correct word was:</p>
+          <div>
+            <p className="text-lg font-bold text-green">{store.word}</p>
+          </div>
+        </div>
+      )}
+      {(store.lost || store.won) && (
+        <>
+          <button onClick={store.startGame}>Play again</button>
+          <button onClick={addWordleStats}>Save your score!</button>
         </>
       )}
       <Keyboard store={store} />
