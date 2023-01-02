8ed0a1c0595f2e28d61ed33824de88935296a7c7
julianabjort <julianabjortiv@gmail.com>
2023-01-02 20:50:08 +0000

    prisma client

--- src/pages/api/wordle-stats.ts
+++ src/pages/api/wordle-stats.ts
@@ -1,8 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient } from "@prisma/client";
 import { prisma } from "../../../lib/prisma";
-
-// const prisma = new PrismaClient()
 
 export default async function handler(
   req: NextApiRequest,
