a84bf47207f279c2ef84ef3aee00d368fe4b0bb1
Laufey Hjaltested <laufeyhjaltested@Laufeys-MacBook-Pro.local>
2023-01-03 14:28:54 +0100

    socket

--- src/pages/groups/[id].tsx
+++ src/pages/groups/[id].tsx
@@ -118,6 +118,29 @@ const tournament = () => {
     updateGuesses();
   }
   useEffect(() => {}, []);
+
+  /**************/
+  // const socket = io();
+
+  // const messages = document.getElementById("messages") as HTMLUListElement;
+  // const form = document.getElementById("form") as HTMLFormElement;
+  // const input = document.getElementById("input") as HTMLInputElement;
+
+  // form.addEventListener("submit", (e) => {
+  //   e.preventDefault();
+  //   if (input.value) {
+  //     socket.emit("chat message", input.value);
+  //     input.value = "";
+  //   }
+  // });
+
+  // socket.on("chat message", (msg: string) => {
+  //   const item = document.createElement("li");
+  //   item.textContent = msg;
+  //   messages.appendChild(item);
+  //   window.scrollTo(0, document.body.scrollHeight);
+  // });
+
   return (
     <div>
       {tournament ? (
